563528d9a335f3b3e46579198a71b13c7b0dad6f
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2024-11-26 17:21:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 import os
 import random
 import discord
+import interactions
 from dotenv import load_dotenv
 
 load_dotenv()
-token = ""
+token = "MTMxMTA3MDczMzU2NDM3OTE2Ng.GBUfEx.1V1VPzP5STE1d-uJzeg61m4p6waTDZczeCctPQ"
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
+    channel = client.get_channel(1311071586530623508) 
+    await channel.send("I have awakened")
 
+@client.event
+async def on_message(message):
+    channel = client.get_channel(1311071586530623508) 
+    if message.author == client.user:
+        return
+    elif message.author.id == 265614755220160512:
+        await channel.send("you stink of the foulest stench i have ever smelt, return from whence you came")
+    else:
+        await channel.send("I hear whispers in the night")
+    
+    
 client.run(token)
